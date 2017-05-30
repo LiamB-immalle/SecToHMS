04c628536cbc37315ba468acedc796b76887bdd1
liamb-immalle <liam.brangers@immalle.be>
2017-05-30 13:09:58 +0200

    Start-up

--- SecToUMS/Program.cs
+++ SecToUMS/Program.cs
@@ -8,8 +8,25 @@ namespace SecToUMS
 {
     class Program
     {
+        static void SecToHoursMinsSecs(int totalSeconds, out int uren, out int minuten, out int seconden)
+        {
+            int rest = 0;
+
+            uren = totalSeconds / (60 * 60);
+            rest = totalSeconds % (60 * 60);
+            minuten = rest / 60;
+            seconden = rest % 60;
+        }
+
         static void Main(string[] args)
         {
+            int h = 0;
+            int m = 0;
+            int s = 0;
+
+            SecToHoursMinsSecs(54324561, out h, out m, out s);
+
+            Console.WriteLine("{0} hours {1} minutes and {2} seconds", h, m, s);
         }
     }
 }
